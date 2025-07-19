e4fbe0a27fcc189974792d53c992c7f0e4fb0e04
Miguel Sanchez <miguelsancor_2005@hotmail.com>
2025-07-19 03:45:30 -0300

    se realiza cambio

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 5173
-  }
+    port: 5180,          // Puerto personalizado
+    host: true,          // Permite conexión externa
+  },
+  preview: {
+    port: 5180,
+    host: true,
+  },
+  base: '/',             // IMPORTANTE para que las rutas no fallen
 })

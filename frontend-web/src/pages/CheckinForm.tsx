fd6df7c7e8ceab426c9172be27d9fe9d41a11f8e
Miguel Sanchez <miguelsancor_2005@hotmail.com>
2025-11-17 01:11:30 -0300

    se realiza modificacion

--- frontend-web/src/pages/CheckinForm.tsx
+++ frontend-web/src/pages/CheckinForm.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://18.206.179.50:4000";
 const DEFAULT_LOCK_ID = Number(import.meta.env.VITE_TTLOCK_LOCK_ID || "0");
 
 type Huesped = {
